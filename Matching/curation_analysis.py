04c407e377daf4d4a4292bee06d073fd27439797
hdclark94 <Worldmap365>
2020-04-22 12:08:17 +0100

    added OF to curation analysis

--- Matching/curation_analysis.py
+++ Matching/curation_analysis.py
@@ -45,17 +45,20 @@ def add_open_field_stuff_to_agreement(agreement_dataframe, sorted_together_of_pa
 
     return new_agreement_dataframe
 
-def run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path, sorted_apart_vr_dir_path,
-                  save_path, figs_path=None, add_of_and_save=False, cohort_mouse=None):
+def run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path,
+                          sorted_apart_vr_dir_path, sorted_apart_of_dir_path, save_path,
+                          figs_path=None, add_of_and_save=False, cohort_mouse=None):
 
     # create list of recordings in sorted folders
     sorted_together_vr_list = [f.path for f in os.scandir(sorted_together_vr_dir_path) if f.is_dir()]
     sorted_apart_vr_list = [f.path for f in os.scandir(sorted_apart_vr_dir_path) if f.is_dir()]
     sorted_together_of_list = [f.path for f in os.scandir(sorted_together_of_dir_path) if f.is_dir()]
+    sorted_apart_of_list = [f.path for f in os.scandir(sorted_apart_of_dir_path) if f.is_dir()]
 
     dataframe_subpath = "/MountainSort/DataFrames/spatial_firing.pkl"
     concat_together = pd.DataFrame()
     concat_apart = pd.DataFrame()
+    concat_apart_of = pd.DataFrame()
 
     # loop over these lists
     for i in range(len(sorted_together_vr_list)):
@@ -65,21 +68,30 @@ def run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_pa
             #look for matching recording
             sorted_together_of = find_match(sorted_together_vr, sorted_together_of_list)
             sorted_apart_vr = find_match(sorted_together_vr, sorted_apart_vr_list)
+            sorted_seperately_of = find_match(sorted_together_vr, sorted_apart_of_list)
 
-            print("Starting curration analysis for between ", get_id(sorted_together_vr), " and ", get_id(sorted_apart_vr))
+            print("Starting curation analysis for between ", get_id(sorted_together_vr), " and ", get_id(sorted_apart_vr))
 
             if os.path.exists(sorted_together_vr + dataframe_subpath):
                 sorted_together = pd.read_pickle(sorted_together_vr + dataframe_subpath)
+                if 'Curated' in list(sorted_together):
+                    sorted_together = sorted_together[sorted_together["Curated"]==1]
                 sorted_together = sorted_together[['cluster_id', 'mean_firing_rate', 'isolation',
                                                    'noise_overlap', 'peak_snr', 'peak_amp']]
+                concat_together = pd.concat([concat_together, sorted_together], ignore_index=True)
+
 
             if os.path.exists(sorted_apart_vr + dataframe_subpath):
                 sorted_seperately = pd.read_pickle(sorted_apart_vr + dataframe_subpath)
                 sorted_seperately = sorted_seperately[['cluster_id', 'mean_firing_rate', 'isolation',
                                                        'noise_overlap', 'peak_snr', 'peak_amp']]
+                concat_apart = pd.concat([concat_apart,sorted_seperately], ignore_index=True)
 
-            concat_together = pd.concat([concat_together, sorted_together], ignore_index=True)
-            concat_apart = pd.concat([concat_apart,sorted_seperately], ignore_index=True)
+            if os.path.exists(sorted_seperately_of + dataframe_subpath):
+                sorted_seperately_of = pd.read_pickle(sorted_seperately_of + dataframe_subpath)
+                sorted_seperately_of = sorted_seperately_of[['cluster_id', 'mean_firing_rate', 'isolation',
+                                                             'noise_overlap', 'peak_snr', 'peak_amp']]
+                concat_apart_of = pd.concat([concat_apart_of, sorted_seperately_of], ignore_index=True)
 
         except Exception as ex:
             print("There was an issue. This is what Python says happened:")
@@ -87,7 +99,7 @@ def run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_pa
             exc_type, exc_value, exc_traceback = sys.exc_info()
             traceback.print_tb(exc_traceback)
 
-    plot_curation_stats(concat_apart, concat_together, figs_path, cohort_mouse)
+    plot_curation_stats(concat_apart, concat_together, concat_apart_of, figs_path, cohort_mouse)
 
 def main():
     print('-------------------------------------------------------------')
@@ -97,35 +109,45 @@ def main():
     # iterate over this list and pick out the spatial dataframes
     figs_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/figs"
 
+    '''
     # cohort 5
     sorted_together_vr_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort5/VirtualReality/M1_sorted"
     sorted_together_of_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort5/OpenField"
-    sorted_apart_vr_dir_path = r"/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort5/VirtualReality/M1_sorted"
+    sorted_apart_vr_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort5/VirtualReality/M1_sorted"
+    sorted_apart_of_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort5/OpenField"
     save_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort5/M1"
-    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path, sorted_apart_vr_dir_path, save_path,
+    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path,
+                          sorted_apart_vr_dir_path, sorted_apart_of_dir_path, save_path,
                   figs_path=figs_path, add_of_and_save=False, cohort_mouse="C5_M1")
 
     sorted_together_vr_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort5/VirtualReality/M2_sorted"
     sorted_together_of_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort5/OpenField"
     sorted_apart_vr_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort5/VirtualReality/M2_sorted"
+    sorted_apart_of_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort5/OpenField"
     save_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort5/M2"
-    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path, sorted_apart_vr_dir_path, save_path,
+    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path,
+                          sorted_apart_vr_dir_path, sorted_apart_of_dir_path, save_path,
                   figs_path=figs_path, add_of_and_save=False, cohort_mouse="C5_M2")
+    '''
 
     # cohort 4
     sorted_together_vr_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort4/VirtualReality/M2_sorted"
     sorted_together_of_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort4/OpenFeild"
+    sorted_apart_of_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort4/OpenFeild"
     sorted_apart_vr_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort4/VirtualReality/M2_sorted"
     save_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort4/M2"
-    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path, sorted_apart_vr_dir_path, save_path,
-                  figs_path=figs_path, add_of_and_save=False, cohort_mouse="C4_M2")
+    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path,
+                          sorted_apart_vr_dir_path, sorted_apart_of_dir_path, save_path,
+                          figs_path=figs_path, add_of_and_save=False, cohort_mouse="C4_M2")
 
     sorted_together_vr_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort4/VirtualReality/M3_sorted"
     sorted_together_of_dir_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort4/OpenFeild"
+    sorted_apart_of_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort4/OpenFeild"
     sorted_apart_vr_dir_path =    "/mnt/datastore/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort4/VirtualReality/M3_sorted"
     save_path = "/mnt/datastore/Harry/Mouse_data_for_sarah_paper/_cohort4/M3"
-    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path, sorted_apart_vr_dir_path, save_path,
-                  figs_path=figs_path, add_of_and_save=False, cohort_mouse="C4_M3")
+    run_curation_analysis(sorted_together_vr_dir_path, sorted_together_of_dir_path,
+                          sorted_apart_vr_dir_path, sorted_apart_of_dir_path, save_path,
+                          figs_path=figs_path, add_of_and_save=False, cohort_mouse="C4_M3")
 
     '''
     # cohort 3 # not done yet
@@ -144,6 +166,7 @@ def main():
                   figs_path=figs_path, add_of_and_save=False, cohort_mouse="C3_M6")
     print("look now")
     '''
+
 
 if __name__ == '__main__':
     main()
